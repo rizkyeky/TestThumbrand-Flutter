44c74a0adf0a7197c23950f299f0889f47c57a05
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-10-19 22:06:20 +0700

    add dummy service in second page

--- lib/bloc/second_bloc.dart
+++ lib/bloc/second_bloc.dart
@@ -1,10 +1,16 @@
 part of 'bloc.dart';
 
 class SecondBloc implements Bloc {
+
+  DummyService dummy = locator.get<DummyService>();
+
   @override
   void dispose() {}
 
   @override
   void init() {}
 
+  List<Map<String, dynamic>> getDataDummies() {
+    return dummy.thumbServices;
+  }
 }
