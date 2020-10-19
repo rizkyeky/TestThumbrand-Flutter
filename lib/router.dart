f74759b46121bfe4fc0948a01595c61cf475e03d
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-10-19 23:22:13 +0700

    modify router

--- lib/router.dart
+++ lib/router.dart
@@ -5,10 +5,10 @@ class XRouter {
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case '/':
-        return MaterialPageRoute(builder: (_) => FirstPage());
-      case '/second':
-        return MaterialPageRoute(builder: (_) => SecondPage());
-      case '/loginpemantau':
+        return MaterialPageRoute(builder: (_) => MainPage());
+      // case '/second':
+      //   return MaterialPageRoute(builder: (_) => SecondPage());
+      // case '/loginpemantau':
         // return MaterialPageRoute(builder: (_) => LogInPemantauPage());
       default:
         return MaterialPageRoute(
