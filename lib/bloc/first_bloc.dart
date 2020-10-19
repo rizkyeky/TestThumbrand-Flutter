c2b6b20e60367cc0b3d860afa318792629c9d941
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-10-19 07:53:21 +0700

    add service and method in first bloc

--- lib/bloc/first_bloc.dart
+++ lib/bloc/first_bloc.dart
@@ -1,11 +1,31 @@
 part of 'bloc.dart';
 
 class FirstBloc implements Bloc {
+
+  final QuoteService _quoteService = locator
+    .get<QuoteService>();
+
+  final Random _random = Random();
+
+  final ValueNotifier<int> notifierColorIndex = ValueNotifier(0);
+  ValueNotifier<Quote> notifierQuote = ValueNotifier(Quote.initial());
+  
   @override
   void dispose() {}
 
   @override
-  void init() {}
+  void init() {
+    notifierQuote.value = _getQuoteFromService();
+  }
 
-  
+  Quote _getQuoteFromService() {
+    return _quoteService.quotes[_random.nextInt(10)];
+  }
+
+  void refersh() {
+    notifierColorIndex.value = List.generate(5, (i) => i)[_random.nextInt(5)];
+    notifierQuote.value = _getQuoteFromService();
+  }
+
+
 }
