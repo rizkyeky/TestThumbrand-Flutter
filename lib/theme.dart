5220a2be518b932536854a967bc9448dd7e0e088
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-10-19 07:52:11 +0700

    update theme

--- lib/theme.dart
+++ lib/theme.dart
@@ -5,29 +5,14 @@ const Color xaccentColor = Color(0xFF03DAC5);
 const Color xblackColor = Color(0xFF505152);
 const Color xwhiteColor = Color(0xFFFFFFFF);
 
-const TextStyle blueTitle = TextStyle(
-    color: xmainColor, fontSize: 24, fontWeight: FontWeight.bold);
+const TextStyle blackTitle = TextStyle(
+    color: xblackColor, fontSize: 24, fontWeight: FontWeight.bold);
 
 const TextStyle whiteTitle = TextStyle(
     color: xwhiteColor, fontSize: 24, fontWeight: FontWeight.bold);
 
 const TextStyle blackSubtitle = TextStyle(
-    color: xblackColor, fontSize: 18, fontWeight: FontWeight.bold);
-
-const TextStyle blueSubtitle = TextStyle(
-    color: xmainColor, fontSize: 18, fontWeight: FontWeight.bold);
+    color: xblackColor, fontSize: 18, fontWeight: FontWeight.normal);
 
 const TextStyle whiteSubtitle = TextStyle(
     color: xwhiteColor, fontSize: 18, fontWeight: FontWeight.normal);
-
-const TextStyle blackContentRegular = TextStyle(
-    color: xblackColor, fontSize: 16, fontWeight: FontWeight.normal);
-
-const TextStyle whiteContentRegular = TextStyle(
-    color: xwhiteColor, fontSize: 16, fontWeight: FontWeight.normal);
-
-const TextStyle whiteNum = TextStyle(
-    color: xwhiteColor, fontSize: 18, fontWeight: FontWeight.bold);
-
-const TextStyle blackNum = TextStyle(
-    color: xblackColor, fontSize: 18, fontWeight: FontWeight.bold);
