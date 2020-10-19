397d944a79862377e8233709b069e6b5e421cda5
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-10-19 22:28:49 +0700

    finish second page

--- lib/view/page/second_page.dart
+++ lib/view/page/second_page.dart
@@ -3,17 +3,16 @@ part of 'page.dart';
 class SecondPage extends Page<SecondBloc> {
 
   @override
-  void dispose() {
-    // TODO: implement dispose
-  }
+  void dispose() {}
 
   @override
-  void init() {
-    // TODO: implement init
-  }
+  void init() {}
 
   @override
   Widget build(BuildContext context) {
+
+    final List<Map<String, dynamic>> datas = _bloc.getDataDummies();
+
     return Scaffold(
       appBar: XTopBar(
         backgroundColor: Colors.transparent,
@@ -37,74 +36,88 @@ class SecondPage extends Page<SecondBloc> {
             const Divider(height: 32),
             Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Stack(
-                  children: [
-                    Card(
-                      color: Colors.white,
-                      elevation: 1,
-                      margin: const EdgeInsets.fromLTRB(24, 24, 0, 0),
-                      child: ConstrainedBox(
-                        constraints: const BoxConstraints.expand(height: 250),
-                        child: Padding(
-                          padding: const EdgeInsets.fromLTRB(6, 6, 6, 0),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            crossAxisAlignment: CrossAxisAlignment.stretch,
-                            children: [
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                                crossAxisAlignment: CrossAxisAlignment.baseline,
-                                children: [
-                                  const SizedBox(height: 120, width: 150,),
-                                  Column(
-                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: [
-                                      SizedBox(
-                                        height: 120,
-                                        width: 150,
-                                        child: Align(
-                                          child: Text('THUMBRAND SERVICE',
-                                            textAlign: TextAlign.left,
-                                            style: blackSubtitle.copyWith(fontWeight: FontWeight.bold)
-                                          ),
-                                        )
-                                      ),
-                                      // const SizedBox(height: 50,),
-                                      Text('\$300\n3 month\n100%', style: blackSubtitle),
-                                    ],
-                                  ),
-                                ],
-                              ),
-                              FlatButton(
-                                onPressed: () {},
-                                child: Text('See More', style: blackSubtitle,),
-                              )
-                            ]
-                          ),
-                        ),
-                      ),
-                    ),
-                    Row(
-                      children: [
-                        const Material(
-                          color: Colors.white,
-                          clipBehavior: Clip.antiAlias,
-                          borderRadius: BorderRadius.all(Radius.circular(75)),
-                          elevation: 1,
-                          child: SizedBox(height: 150, width: 150,),
-                        ),
-                        
-                      ],
-                    )
-                  ]
-                ),
-              ],
+              children: List.generate(
+                datas.length, (index) => 
+                  MyCard(data: datas[index],)
+              )
             )
           ],
         ),
       ),
+    );
+  }
+}
+
+class MyCard extends StatelessWidget {
+
+  final Map<String, dynamic> data;
+
+  const MyCard({
+    this.data
+  });
+
+  @override
+  Widget build(BuildContext context) {
+
+    final List lsData = data.values.toList();
+
+    return Stack(
+      children: [
+        Card(
+          color: Colors.white,
+          elevation: 1,
+          margin: const EdgeInsets.fromLTRB(24, 24, 0, 24),
+          child: ConstrainedBox(
+            constraints: const BoxConstraints.expand(height: 250),
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(6, 6, 6, 0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.baseline,
+                    children: [
+                      const SizedBox(height: 120, width: 150,),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                            height: 120,
+                            width: 150,
+                            child: Align(
+                              child: Text('${lsData[0]}',
+                                textAlign: TextAlign.left,
+                                style: blackSubtitle.copyWith(fontWeight: FontWeight.bold)
+                              ),
+                            )
+                          ),
+                          Text('\$${lsData[1]}\n${lsData[2]} month\n${lsData[3]}%', 
+                            style: blackSubtitle
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  FlatButton(
+                    onPressed: () {},
+                    child: const Text('See More', style: blackSubtitle,),
+                  )
+                ]
+              ),
+            ),
+          ),
+        ),
+        const Material(
+          color: Colors.white,
+          clipBehavior: Clip.antiAlias,
+          borderRadius: BorderRadius.all(Radius.circular(75)),
+          elevation: 1,
+          child: SizedBox(height: 150, width: 150,),
+        )
+      ]
     );
   }
 }
