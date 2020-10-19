b33256ac40f74d71e0936af70c936838fe5840b0
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-10-19 19:35:32 +0700

    add widgets and update second page

--- lib/view/page/second_page.dart
+++ lib/view/page/second_page.dart
@@ -26,7 +26,7 @@ class SecondPage extends Page<SecondBloc> {
         textStyle: blackTitle,
       ),
       body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16),
+        padding: const EdgeInsets.all(24),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
@@ -34,11 +34,49 @@ class SecondPage extends Page<SecondBloc> {
               style: blackTitle.copyWith(fontSize: 30),
               textAlign: TextAlign.left,
             ),
-            SizedBox(height: 16,),
-            XTextField(
+            const SizedBox(height: 16,),
+            const XTextField(
               prefixIcon: Icon(Icons.search),
             ),
-            Divider(height: 32, thickness: 2,)
+            const Divider(height: 32),
+            Column(
+              children: [
+                Stack(
+                  alignment: AlignmentDirectional.topCenter,
+                  children: [
+                    Card(
+                      child: Container(height: 200, margin: EdgeInsets.symmetric(horizontal: 30),),
+                    ),
+                    Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Material(
+                              clipBehavior: Clip.antiAlias,
+                              borderRadius: BorderRadius.all(Radius.circular(100)),
+                              elevation: 10,
+                              child: Container(height: 100, width: 100,),
+                            ),
+                            SizedBox(
+                              width: 150,
+                              child: Text('THUMBRAND SERVICE',
+                                textAlign: TextAlign.left,
+                                style: blackSubtitle.copyWith(fontWeight: FontWeight.bold)
+                              )
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 50,),
+                          Text('\$300\n3mo.\n100%',
+                            textAlign: TextAlign.left,
+                            style: blackSubtitle.copyWith(fontWeight: FontWeight.bold)),
+                      ],
+                    )
+                  ]
+                ),
+              ],
+            )
           ],
         ),
       ),
