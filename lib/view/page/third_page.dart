05231ce517e15b526dd71a691c3b1023fa4b60a7
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-10-19 22:49:53 +0700

    create third page

---
+++ lib/view/page/third_page.dart
@@ -0,0 +1,17 @@
+part of 'page.dart';
+
+class ThirdPage extends Page<ThirdBloc> {
+
+  @override
+  void dispose() {}
+
+  @override
+  void init() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
