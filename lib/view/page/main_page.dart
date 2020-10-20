a78495e95b2d165ccaeab95ce1d303b18501e164
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-10-20 08:09:50 +0700

    modify bottom nav icon in main page

--- lib/view/page/main_page.dart
+++ lib/view/page/main_page.dart
@@ -30,9 +30,18 @@ class MainPage extends StatelessWidget {
           showUnselectedLabels: false,
           currentIndex: value,
           items: const [
-            BottomNavigationBarItem(icon: Icon(Icons.messenger, color: Colors.grey), label: 'Qoutes'),
-            BottomNavigationBarItem(icon: Icon(Icons.explore, color: Colors.grey), label: 'Explore'),
-            BottomNavigationBarItem(icon: Icon(Icons.bar_chart, color: Colors.grey), label: 'Graph'),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.messenger, color: Colors.grey), label: 'Qoutes',
+              activeIcon: Icon(Icons.messenger, color: xblackColor,)
+              ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.explore, color: Colors.grey), label: 'Explore',
+              activeIcon: Icon(Icons.explore, color: xblackColor,)
+              ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.bar_chart, color: Colors.grey), label: 'Graph',
+              activeIcon: Icon(Icons.bar_chart, color: xblackColor,)
+              ),
             
           ],
           onTap: (index) {
