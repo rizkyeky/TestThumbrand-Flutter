1c0808130e7cded78790d4cc2e8e47d9cfcd3d24
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-10-20 09:19:39 +0700

    update service

--- lib/service/service.dart
+++ lib/service/service.dart
@@ -3,7 +3,12 @@
 import 'dart:convert';
 
 import 'package:flutter/services.dart';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 import 'package:test_thumbrand/model/model.dart';
 
 part 'quote_service.dart';
 part 'dummy_service.dart';
+part 'graph_service.dart';
