6f930a7ae1c77b67519c7da6e4e5d0be5a819ec8
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-10-20 21:02:46 +0700

    update and fix graph service

--- lib/service/graph_service.dart
+++ lib/service/graph_service.dart
@@ -8,11 +8,12 @@ class GraphService {
   final Map<String, Map<String, int>> _bar = {};
   Map<String, Map<String, int>> get bar => _bar;
 
-  final CollectionReference _graphCollection = FirebaseFirestore
-    .instance.collection('graph');
+  CollectionReference _graphCollection;
 
   Future<void> init() async {
     await Firebase.initializeApp();
+    _graphCollection = FirebaseFirestore.instance.collection('graph');
+    
     await _getPie();
     await _getBar();
   }
@@ -20,12 +21,19 @@ class GraphService {
   Future<void> _getPie() async {
     final DocumentSnapshot snapshot = await _graphCollection
       .doc('pie').get();
-    _pie.addAll(snapshot.data() as Map<String, int>);
+    _pie.addAll(Map<String, int>.from(snapshot.data()));
   }
 
   Future<void> _getBar() async {
     final DocumentSnapshot snapshot = await _graphCollection
       .doc('bar').get();
-    _bar.addAll(snapshot.data() as Map<String, Map<String, int>>);
+
+    final Map<String, Map<String, int>> data = {};
+
+    snapshot.data().forEach((key, value) {
+      data[key] = Map<String, int>.from(value as Map);
+    });
+    
+    _bar.addAll(data);
   }
 }
