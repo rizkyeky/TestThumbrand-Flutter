ef6d0c8d2ac794437e96af1cd51a224420266d83
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-10-20 09:19:32 +0700

    add get method in third bloc

--- lib/bloc/third_bloc.dart
+++ lib/bloc/third_bloc.dart
@@ -1,11 +1,20 @@
 part of 'bloc.dart';
 
 class ThirdBloc implements Bloc {
+
+  final GraphService _graphService = locator.get<GraphService>();
+
   @override
   void dispose() {}
 
   @override
   void init() {}
 
-  
+  Map<String, int> getPieFromService() {
+    return _graphService.pie;
+  }
+
+  Map<String, Map<String, int>> getBarFromService() {
+    return _graphService.bar;
+  }
 }
