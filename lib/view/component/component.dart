055f93b269119cece7b23ba005f085497e83e11e
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-10-21 08:06:24 +0700

    update component

--- lib/view/component/component.dart
+++ lib/view/component/component.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:test_thumbrand/theme.dart';
 
-part 'xbutton.dart';
 part 'xtextfield.dart';
 part 'xtopbar.dart';
-part 'xtopprogressindicator.dart';
-part 'xbox.dart';
-part 'xdropdown.dart';
+part 'xrotateicon.dart';
