dbcbd519bb716140df830eed321f305e0539b5c8
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-10-21 08:06:44 +0700

    create rotate icon component

---
+++ lib/view/component/xrotateicon.dart
@@ -0,0 +1,34 @@
+part of 'component.dart';
+
+class XRotatedIcon extends StatefulWidget {
+
+  final Icon icon;
+  final int duration;
+  final bool start;
+
+  const XRotatedIcon({
+    Key key,
+    this.icon,
+    this.duration,
+    this.start,
+  }) : super(key: key);
+
+  @override
+  _XAnimatedIconState createState() => _XAnimatedIconState();
+}
+
+class _XAnimatedIconState extends State<XRotatedIcon> with TickerProviderStateMixin {
+  @override
+  Widget build(BuildContext context) {  
+    return RotationTransition(
+      turns: CurvedAnimation(
+        parent:  AnimationController(
+          duration: Duration(seconds: widget.duration ?? 2),
+          vsync: this,
+        )..forward(),
+        curve: Curves.easeOut,
+      ),
+      child: widget.icon,
+    );
+  }
+}
